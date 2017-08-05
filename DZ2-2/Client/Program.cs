69c5f09953095f83331c26f2ea47eade099111f3
Ph@nt0m-X <phantom-x@inbox.ru>
2017-08-05 18:14:33 +0300

    DZ2

---
+++ DZ2-2/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.ServiceReference1;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client proxy = new Service1Client();
+            int choice = -1;
+            string path;
+            bool goOn = true;
+            string[] result;
+            do
+            {
+                Console.WriteLine("1 - Просмотреть содержимое папки\n0 - Выход");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        goOn = false;
+                        break;
+                    case 1:
+                        Console.Write("Введите путь: ");
+                        path = Console.ReadLine();
+                        result = proxy.getContent(path);
+                        foreach (var item in result)
+                            Console.WriteLine(item);
+                        break;
+                    default:
+                        Console.WriteLine("Неверный вывод");
+                        break;
+                }
+            } while (goOn);
+        }
+    }
+}
---
+++ DZ2-2/WCF_Service_dll/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.IO;
+
+namespace WCF_Service_dll
+{
+    public class Service1 : IService1
+    {
+        public List<string> getContent(string path)
+        {
+            List<string> result = new List<string>();
+            if (!Directory.Exists(path))
+            {
+                result.Add("Path not found");
+                return result;
+            }
+            string[] temp = Directory.GetDirectories(path);
+            foreach (var item in temp)
+                result.Add(item);
+            temp = Directory.GetFiles(path);
+            foreach (var item in temp)
+                result.Add(item);
+            return result;
+        }
+    }
+}
